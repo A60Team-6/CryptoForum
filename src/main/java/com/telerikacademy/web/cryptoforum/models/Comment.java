60c4db66bad072e746945b71f65ed7e2c9a515b9
MarioBoykov <166829333+MarioBoykov@users.noreply.github.com>
2024-07-22 13:06:55 +0300

    Created Comment update

--- src/main/java/com/telerikacademy/web/cryptoforum/models/Comment.java
+++ src/main/java/com/telerikacademy/web/cryptoforum/models/Comment.java
@@ -30,5 +30,62 @@ public class Comment {
     @JoinColumn(name = "parent_id")
     private Comment parent;
 
+    public Comment() {
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDate getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDate updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public Comment getParent() {
+        return parent;
+    }
+
+    public void setParent(Comment parent) {
+        this.parent = parent;
+    }
 }
