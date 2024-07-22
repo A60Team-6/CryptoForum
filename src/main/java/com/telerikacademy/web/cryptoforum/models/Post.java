d608b02d92789b2abbce3af5e76de9bc4a84b530
Asus <i.t.nachkov@abv.bg>
2024-07-22 12:38:23 +0300

    Post Model Created

--- src/main/java/com/telerikacademy/web/cryptoforum/models/Post.java
+++ src/main/java/com/telerikacademy/web/cryptoforum/models/Post.java
@@ -1,4 +1,103 @@
 package com.telerikacademy.web.cryptoforum.models;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+
+import java.security.Timestamp;
+import java.util.Objects;
+
 public class Post {
+
+    @Id
+    private int id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+
+    private String title;
+
+    private String content;
+
+    private int likes = 0;
+    private int dislikes = 0;
+
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    public Post() {
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public @NotNull User getUser() {
+        return user;
+    }
+
+    public void setUser(@NotNull User user) {
+        this.user = user;
+    }
+
+    public int getDislikes() {
+        return dislikes;
+    }
+
+    public void setDislikes(int dislikes) {
+        this.dislikes = dislikes;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
