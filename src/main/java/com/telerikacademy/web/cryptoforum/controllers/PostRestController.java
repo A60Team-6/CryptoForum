9962bc05fefe02410da76a7afa8ceac85d966947
MarioBoykov <166829333+MarioBoykov@users.noreply.github.com>
2024-07-23 16:00:13 +0300

    PostDto class created and PostRestController

---
+++ src/main/java/com/telerikacademy/web/cryptoforum/controllers/PostRestController.java
@@ -0,0 +1,11 @@
+package com.telerikacademy.web.cryptoforum.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostRestController {
+
+
+}
